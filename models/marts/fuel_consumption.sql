dc1d4c6b2560d013096279b5acf5c21d367b0682
olivier calame <olivier.calame@groupama.com>
2024-11-28 15:38:43 +0000

    ajout fuel_consumption

---
+++ models/marts/fuel_consumption.sql
@@ -0,0 +1,7 @@
+SELECT
+  device_id,
+  trip_id,
+  avg(consumption_km_by_liter) AS avg_cons_km_by_liter
+FROM {{ ref('stg_telematics') }}
+where revolutions_per_min > 0
+GROUP  BY device_id , trip_id 
